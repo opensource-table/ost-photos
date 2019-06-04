a63a39e3e4a888fc43923649669ef2e682063ba6
Wayne Sun <wtse.sun@gmail.com>
2019-06-04 15:46:02 -0700

    implement readme and createOneData

---
+++ sdc_project/dataGenerator/dataGenerator.js
@@ -0,0 +1,26 @@
+// `id`  //1 to end
+//  `restaurant` // name of restaurant
+//  `url` // Facker url
+//  `timestamp` // YYYY-MM-DD
+//  `unrelated_report` // ??
+//  `inappropriate_report` // ???
+//  `dislike` // 0 or 1
+
+//  1, 'Rempel Group', 'https://resizer.otstatic.com/v2/photos/large/24947294.jpg', '1977-02-12', 6, 3, 0
+
+const faker = require('faker');
+const casual = require('casual');
+
+console.log(faker.date.past(10));
+
+function createOneData(id) {
+  let res = { id, };
+  res.restaurant = faker.company.companyName();
+  res.url = faker.image.food();
+  res.timestamp = casual.date(format = 'YYYY-MM-DD');
+  res.unrelated_report = faker.random.number(10);
+  res.inappropriate_report = faker.random.number(10);
+  res.dislike = faker.random.number(1);
+  return res;
+}
+
