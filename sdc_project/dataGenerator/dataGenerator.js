5491c5b1c8f11e503808e4fdfb726d2dd2748168
Wayne Sun <wtse.sun@gmail.com>
2019-06-05 22:12:19 -0700

    refactor timer and createMass function

--- sdc_project/dataGenerator/dataGenerator.js
+++ sdc_project/dataGenerator/dataGenerator.js
@@ -4,7 +4,7 @@ const faker = require('faker');
 const casual = require('casual');
 const fs = require('fs');
 
-let wstream = fs.createWriteStream('out.csv', {flags: 'a'});
+let wstream = fs.createWriteStream('data.csv', {flags: 'a'});
 
 //String type data
 const createStringData = function(room_id, pic_id) {
@@ -21,45 +21,45 @@ wstream.write("id_r,pic_id,restaurant,url,timestamp,dislike\n");
 wstream.end();
 
 
-const createMassive = function (from, to) {
+const createMassive = function (start, end) {
   //timer start
   wstream = fs.createWriteStream('out.csv', {flags: 'a'});
-  let start = Date.now();
+  let startTime = Date.now();
   console.log('creating csv...');
   //writing csv
-  let i = from;
+  let i = start;
   handler();
   function handler() {
     let ok = true;
     do {
       
-
       //counting.....
-      if (i % 100000 === 0) {
-        let temp = i / 1000000;
+      if (i % 10000 === 0) {
+        let temp = i / 100000;
         console.log('hits ' + temp + ' million');
       }
-
+      let stringData = '';
       //writing.......
-      let randomNum = casual.integer(from = 10, to = 13);
+      let randomNum = casual.integer(from = 10, to = 12);
       for (let j = 0; j < randomNum; j++) {
-        let stringData = createStringData(i, j);
-        ok = wstream.write(stringData);
+        stringData += createStringData(i, j);
       }
-
+      ok = wstream.write(stringData);
       i ++;
-    } while ( i < to && ok);
+    } while ( i < end && ok);
 
-    if( i < to ) {
+    //timer stop
+    if (i === end) {
+      let period = Number((Date.now() - startTime) / 1000).toFixed(2);
+      console.log(period + ' seconds');
+    }
+
+    if ( i < end ) {
       wstream.once('drain', handler);
     }
   }
-  
-  //timer stop
-  let period = Number((Date.now() - start)/1000).toFixed(2);
-  console.log(period + ' seconds');
 };
 
 //test
-createMassive(0, 1000);
+createMassive(0, 10000000);
 
