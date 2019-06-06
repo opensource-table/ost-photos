f1a14cd8014768374b0d6946ab109c34d241ddf5
Wayne Sun <wtse.sun@gmail.com>
2019-06-05 19:15:09 -0700

    add 100m generator

--- sdc_project/dataGenerator/dataGenerator.js
+++ sdc_project/dataGenerator/dataGenerator.js
@@ -1,59 +1,60 @@
+/* eslint-disable func-style */
 
 const faker = require('faker');
 const casual = require('casual');
 const fs = require('fs');
 
-let wstream = fs.createWriteStream('out.csv')
-
-// const csvWriter = require('csv-write-stream');
-// let writer = csvWriter({sendHeaders: false});
-
-//Object type data
-const createOneData = function (room_id, pic_id) {
-  let oneImage = { id: room_id, pic_id };
-  oneImage.restaurant = faker.company.companyName();
-  oneImage.url = faker.image.food();
-  oneImage.timestamp = casual.date(format = 'YYYY-MM-DD');
-  oneImage.unrelated_report = faker.random.number(10);
-  oneImage.inappropriate_report = faker.random.number(10);
-  oneImage.dislike = faker.random.number(1);
-  return oneImage;
-};
+let wstream = fs.createWriteStream('out.csv', {flags: 'a'});
 
 //String type data
 const createStringData = function(room_id, pic_id) {
   return `${room_id},\
 ${pic_id},\
-"${faker.company.companyName()}",\
+${faker.lorem.word()},\
 ${faker.image.food()},\
 ${casual.date(format = 'YYYY-MM-DD')},\
-${faker.random.number(10)},\
-${faker.random.number(10)},\
 ${faker.random.number(1)}
 `
 }
 
+wstream.write("id_r,pic_id,restaurant,url,timestamp,dislike\n");
+wstream.end();
 
-const createMassive = function (number) {
+
+const createMassive = function (from, to) {
   //timer start
+  wstream = fs.createWriteStream('out.csv', {flags: 'a'});
   let start = Date.now();
-
-  console.log('start creating csv...');
-
+  console.log('creating csv...');
   //writing csv
-  wstream.write("id,pic_id,restaurant,url,timestamp,unrelated_report,inappropriate_report,dislike\n");
-  for (let i = 0; i < number; i++) {
-    // let data = createOneData(Math.floor(i / 10), i % 10);
-    // wstream.write(data);
-    let stringData = createStringData(Math.floor(i / 10), i % 10);
-    wstream.write(stringData);
-  }
+  let i = from;
+  handler();
+  function handler() {
+    let ok = true;
+    do {
+      i ++;
 
+      //counting.....
+      if (i % 100000 === 0) {
+        let temp = i / 1000000;
+        console.log('hits ' + temp + ' million');
+      }
+
+      //writing.......
+      let stringData = createStringData(Math.floor(i / 10), i % 10);
+      ok = wstream.write(stringData);
+    } while ( i < to && ok);
+
+    if( i < to ) {
+      wstream.once('drain', handler);
+    }
+  }
+  
   //timer stop
-  wstream.end();
   let period = Number((Date.now() - start)/1000).toFixed(2);
   console.log(period + ' seconds');
 };
 
+//test
+createMassive(0, 100000000);
 
-createMassive(100);
