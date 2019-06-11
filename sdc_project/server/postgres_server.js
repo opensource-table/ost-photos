e1dcc3ec060362c01059e549b77dd2d079d8fa6b
Wayne Sun <wtse.sun@gmail.com>
2019-06-10 19:21:14 -0700

    installed new relic

--- sdc_project/server/postgres_server.js
+++ sdc_project/server/postgres_server.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 3006;
 
+app.use(bodyParser.json());
+app.use(cors());
+
+//Database connection
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -13,27 +17,39 @@ const pool = new Pool({
   database: 'opentable'
 });
 
+//Connect to PostgreSQL
+pool.connect((err, client, release) => {
+  if(err) {
+    res.status(500).send(err);
+    return;
+  }
+});
 
 
-app.use(bodyParser.json());
-app.use(cors());
-
+//GET REQUEST
 app.get('/photos/:id_r', (req, res) => {
   let id_r = req.params.id_r; 
-  pool.connect((err, client, release) => {
-    if(err) {
-      res.status(500).send(err);
-      return;
-    }
-  });
   pool.query(`SELECT * FROM photos WHERE id_r = ${id_r}`)
     .then((data) => {
       res.status(200).send(data.rows);
     })
     .catch((err) => {
       res.status(500).send(err);
+    });
+});
+
+//POST REQUEST
+app.post('/photos/:id_r', (req, res) => {
+  let id_r = req.params.id_r;
+  let newPhoto = req.body;
+  // res.status(200).send(newPhoto);
+  pool.query(`INSERT INTO photos(id, id_r, pic_id, restaurant, timestamp, url, dislike) VALUES (${newPhoto.id}, ${id_r}, ${newPhoto.pic_id}, ${newPhoto.restaurant}, ${newPhoto.timestamp}, ${newPhoto.url}, ${newPhoto.dislike});`)
+    .then(() => {
+      res.status(201).send('success');
     })
-  
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 });
 
 app.listen(PORT, () => console.log('Start to listen on port ' + PORT));
