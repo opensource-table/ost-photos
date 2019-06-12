03cc561ae566737cf84ccad7a7a5e9f64b2c04c7
Wayne Sun <wtse.sun@gmail.com>
2019-06-11 19:48:42 -0700

    implement stress test

--- sdc_project/dataGenerator/dataGenerator.js
+++ sdc_project/dataGenerator/dataGenerator.js
@@ -12,24 +12,46 @@ const bar = new cliProgress.Bar({}, cliProgress.Presets.shades_classic);
 // const uuidv1 = require('uuid/v1');
 
 const STARTPOINT = 0;
-const ENDPOINT = 10000000; //times 10 -> data
-const DATANAME = 'data1.csv'
+const ENDPOINT = 100; //times 10 -> data
+const DATANAME = 'data2.csv'
 
 let wstream = fs.createWriteStream(DATANAME);
 let counter = 0;
+
+const resUrl = ['http://wang-guan.com/o/01.jpg',
+  'http://wang-guan.com/o/02.jpg',
+  'http://wang-guan.com/o/03.jpg',
+  'http://wang-guan.com/o/04.jpg',
+  'http://wang-guan.com/o/05.jpg',
+  'http://wang-guan.com/o/06.jpg',
+  'http://wang-guan.com/o/07.jpg',
+  'http://wang-guan.com/o/08.jpg',
+  'http://wang-guan.com/o/09.jpg',
+  'http://wang-guan.com/o/10.jpg',
+  'http://wang-guan.com/o/11.jpg',
+  'http://wang-guan.com/o/12.jpg',
+  'http://wang-guan.com/o/13.jpg',
+  'http://wang-guan.com/o/14.jpg',
+  'http://wang-guan.com/o/15.jpg',
+  'http://wang-guan.com/o/16.jpg',
+  'http://wang-guan.com/o/17.jpg',
+  'http://wang-guan.com/o/18.jpg',
+  'http://wang-guan.com/o/19.jpg',
+  'http://wang-guan.com/o/20.jpg'];
+
+
 //String type data
-const createStringData = function(id, room_id, pic_id) {
+const createStringData = function(id, room_id, pic_id, resName, url) {
   return `${id},\
 ${room_id},\
+${resName},\
 ${pic_id},\
-${faker.lorem.word()},\
 ${casual.date(format = 'YYYY-MM-DD')},\
-${faker.image.food()},\
-${faker.random.number(1)}
+${url}
 `
 }
 
-wstream.write("id,id_r,pic_id,restaurant,timestamp,url,dislike\n");
+wstream.write("id,id_r,resName,pic_id,timestamp,url\n");
 wstream.end();
 
 const createMassive = function (start, end) {
@@ -51,8 +73,9 @@ const createMassive = function (start, end) {
       let stringData = '';
       //writing.......
       let randomNum = casual.integer(from = 10, to = 12);
+      let tempName = casual.word;
       for (let j = 0; j < randomNum; j++) {
-        stringData += createStringData(counter, i, j);
+        stringData += createStringData(counter, i, j, tempName, resUrl[j]);
         counter += 1;
       }
       ok = wstream.write(stringData);
--- sdc_project/database/postgres.sql
+++ sdc_project/database/postgres.sql
@@ -6,16 +6,14 @@ CREATE DATABASE opentable;
 CREATE TABLE photos (
   id INTEGER PRIMARY KEY NOT NULL,
   id_r INTEGER NOT NULL,
+  resName VARCHAR NOT NULL,
   pic_id INTEGER NOT NULL,
-  restaurant VARCHAR NOT NULL,
   timestamp VARCHAR NOT NULL,
-  url VARCHAR NOT NULL,
-  dislike INTEGER NOT NULL
+  url VARCHAR NOT NULL
 );
 
 CREATE INDEX rest_id ON photos(id_r);
-CREATE INDEX rest_name ON photos(restaurant);
-CREATE INDEX rest_time ON photos(timestamp);
+CREATE INDEX rest_name ON photos(resName);
 
-COPY photos(id, id_r, pic_id, restaurant, timestamp, url, dislike) 
+COPY photos(id, id_r, resName, pic_id, timestamp, url) 
 FROM '/Users/SunWei-Tse/Google/HackReactor/opentable/ost_photos/data1.csv' DELIMITER ',' CSV HEADER;
