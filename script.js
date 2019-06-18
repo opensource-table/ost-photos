48aa16232a44e28e381fa99442e48dae1bbaeaf1
Wayne Sun <wtse.sun@gmail.com>
2019-06-18 11:29:05 -0700

    edit port on postgres server

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ import http from "k6/http";
 const PORT = 3006;
 
 export let options = {
-  vus: 2000,
-  rps: 2000,
+  vus: 300,
+  rps: 300,
   duration: "5m"
 }
 
 export default function () {
-  let res = http.get(`http://localhost:${PORT}/photos`);
+  // let res = http.get(`http://localhost:${PORT}/photos`);
+  let res = http.get(`http://52.15.114.103:3006/`);
   check(res, {
     "is status 200": (r) => r.status === 200
   });
